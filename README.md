5a35fbdd14a108a09b837904f9b7f617cc5074bc
David Hartsough <hartsoughdavid@gmail.com>
2019-10-08 23:52:09 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Friendly Feed
+
+**[Chrome extension](https://chrome.google.com/webstore/detail/friendly-feed/dohehejmmbcfejempphbkcehfhinodem)**
+
+No more trending, related, suggested, or sponsored content; no more activity streams; no more ads; just updates from friends and pages.
+
+Make your Facebook feed friendly. This extension simply hides several annoying elements from Facebook, including trending topics, "related" and "suggested" content or groups, "people you may know," ads, etc. Because this is intended to make your feed only show posts written by your friends and pages you like, this extension will also hide all feed items that include "this" in the title, which will effectively hide a majority of your friends' activity. These posts are typically titled/prefixed with "John Doe liked this" or "Jane Doe commented on this." The idea is to create a Facebook feed of only your friends' posts.
---
+++ src/friendly-feed.js
@@ -0,0 +1,62 @@
+const firstKeywords = [
+  " replied ",
+  " liked ",
+  " commented on this.",
+  " commented on a post from ",
+  " reacted "
+];
+const secondKeywords = [
+  " likes ",
+  " is going to an event.",
+  " is interested in an event.",
+  " followed ",
+  " are now friends.",
+  " like "
+];
+function replacePost(child, text) {
+  const para = document.createElement("p");
+  para.style.margin = "4px 16px";
+  const node = document.createTextNode(text);
+  para.appendChild(node);
+  child.parentNode.replaceChild(para, child);
+}
+// const removePost = child => child.parentNode.removeChild(child);
+function friendlyFeed() {
+  // "Suggested Post"
+  Array.from(document.getElementsByClassName("_m8d")).forEach(x => {
+    const child = x.parentElement.parentElement.parentElement.parentElement;
+    replacePost(child, 'suggested post');
+  });
+  // "Suggested Post" and "A Video You May Like"
+  Array.from(document.getElementsByClassName("_5g-l")).forEach(x => {
+    const child = x.parentElement.parentElement.parentElement;
+    replacePost(child, 'suggested post2');
+  });
+  // "Sponsored"
+  Array.from(document.getElementsByClassName("o_niairssnk")).forEach(x => {
+    const child = x.closest("._5pcr.userContentWrapper");
+    replacePost(child, 'sponsored post');
+  });
+  // Anything with a text section above the post ("Suggested for you")
+  Array.from(document.getElementsByClassName("p_niairspyz")).forEach(x => {
+    const child = x.parentElement.parentElement.parentElement;
+    replacePost(child, x.textContent);
+  });
+  // Keywords
+  Array.from(document.getElementsByClassName("fwn fcg")).forEach(x => {
+    const text = x.firstChild.textContent;
+    if (firstKeywords.some(v => text.indexOf(v) >= 0)) {
+      const p = x.parentElement.parentElement.parentElement;
+      if (!p.className.includes("_42ef")) {
+        replacePost(p.parentElement, `(${text})`);
+      }
+    } else if (secondKeywords.some(v => text.indexOf(v) >= 0)) {
+      const child = x.parentElement.parentElement.parentElement.parentElement
+        .parentElement.parentElement.parentElement.parentElement.parentElement
+        .parentElement.parentElement.parentElement;
+      replacePost(child, text);
+    }
+  });
+}
+friendlyFeed();
+setInterval(friendlyFeed, 2000);
